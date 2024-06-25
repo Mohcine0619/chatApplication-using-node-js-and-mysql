74e12542338e30541981401c9e3aca9ce58a5306
Mohcine0619 <akkadmohcine07@gmail.com>
2024-06-25 01:15:20 +0100

    ifcon added

--- server.js
+++ server.js
@@ -14,7 +14,16 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 // Set up Handlebars view engine
-app.engine('handlebars', exphbs.engine());
+app.engine('handlebars', exphbs.engine({
+  helpers: {
+    ifCond: function (v1, v2, options) {
+      if (v1 === v2) {
+        return options.fn(this);
+      }
+      return options.inverse(this);
+    }
+  }
+}));
 app.set('view engine', 'handlebars');
 
 // Middleware to parse JSON bodies
